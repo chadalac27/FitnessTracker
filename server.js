f19ed95d16eda0d648dba69b2394b9a84b981128
chadalac27 <chadcoleman1234@gmail.com>
2020-12-24 14:17:42 -0800

    Updated the server.js to include mongoose, added mongoose connection, added api routes

--- server.js
+++ server.js
@@ -17,14 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout", 
-  { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+  process.env.MONGODB_URI || "mongodb://localhost/workout");
 
 app.get("/exercise", (req,res) => {
     res.sendFile(path.join(__dirname, "./public/exercise.html"))
